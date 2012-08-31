a44078bf1a7025738bf958c72951b7263270e3e3
Kyle Temkin <ktemkin@binghamton.edu>
2012-08-31 19:35:34 -0400

    NOBUG import from cvs

--- block_unanswered_discussions.php
+++ block_unanswered_discussions.php
@@ -1,74 +1,66 @@
-<?php
+<?php //$Id: block_unanswered_discussions.php,v 1.4 2011/02/22 02:20:55 deraadt Exp $
+      // Modified by Michael de Raadt from 2010072000
 
 class block_unanswered_discussions extends block_base {
+
     // Default Configuration
-    var $defaultconfig = array(
-        // Default block title
-        'title' => 'Unanswered Discussions',
-
-        // The number of posts of each section to display
-        'limit' => array(
-            2, // Random Unanswered Posts
-            2, // Oldest Unanswered Posts
-            2  //   Your Unanswered Posts
-            ),
-
-        // Change the following to false to disable the section marked
-        'enabled' => array(
-            true, // Random Unanswered Posts
-            true, // Oldest Unanswered Posts
-            true  //   Your Unanswered Posts
-            ),
-
-        // Whether to show an image next to each entry
-        'show_image' => true,
-
-        // Use this to exclude fora from the selection process. Useful if you don't
-        // want your "News" or "Announcements" forum showing up.
-        //
-        // You'll have to put the forum ids in here manually, though, seperated by a
-        // comma (,). Open the forums page for your course and look at the link to the
-        // forum you want to exclude. It should have an "f=<number>" at the end of it.
-        // That number is the forum id.
-        //
-        // You can put fora from any course in here, they all have different ID's, so
-        // mixing and matching is fine.
-        //
-        // It should end up looking something like:
-        // var $exclude = array(4, 7, 11);
-        // where forum 4 and 7 may be in course 2 and forum 11 may be in course 5.
-        'exclude' => array()
+    var $defaultLimits = array(
+        'randomposts' => 0, // Random Unanswered Posts
+        'oldestposts' => 2, // Oldest Unanswered Posts
+        'yourposts'   => 2  // Your Unanswered Posts
     );
-
+    var $maxSubjectLength = 20; // characters
+    var $maxShowOption = 10; // messages
+    var $queryLimit = 50; // messages
+    
+    //--------------------------------------------------------------------------
     function init() {
         $this->title = get_string('unanswereddiscussions', 'block_unanswered_discussions');
-        $this->version = 2005091300;
     }
 
-    function preferred_width() {
-        return 210;
+    //--------------------------------------------------------------------------
+    function applicable_formats() {
+        return array('course-view' => true);
+    }
+    
+    //--------------------------------------------------------------------------
+    function instance_allow_multiple() {
+        return true;
     }
 
+    //--------------------------------------------------------------------------
+    function has_config() {
+        return false;
+    }
+
+    //--------------------------------------------------------------------------
     function specialization() {
+        global $COURSE, $DB;
+
+        // Create the config object
         if (!isset($this->config)) {
             $this->config = new stdClass;
-        } else {
-            if (!isset($this->config->show_image)) {
-                $this->config->show_image = false;
+        }
+
+        // Set up the default config values
+        foreach($this->defaultLimits as $name => $value) {
+            if (!isset($this->config->{$name})) {
+                $this->config->{$name} = $value;
             }
         }
 
-        foreach($this->defaultconfig as $name => $value) {
-            if (!isset($this->config->$name)) {
-                $this->config->$name = $value;
+        // Excluded News forums by default
+        if(!isset($this->config->exclude)) {
+            $this->config->exclude = array();
+            if($newsForums = $DB->get_records_select('forum',"course=\"$COURSE->id\" AND type=\"news\"")) {
+                foreach($newsForums as $key => $forum) {
+                    $this->config->exclude[] = $forum->id;
+                }
             }
-        }
-
-        if (isset($this->config->title)) {
-            $this->title = $this->config->title;
         }
     }
 
+    //--------------------------------------------------------------------------
     function get_data($course = 0) {
         global $CFG, $USER, $DB;
 
@@ -76,103 +68,80 @@ class block_unanswered_discussions extends block_base {
         if (!empty($this->discussions)) {
             return $this->discussions;
         }
-
         $this->discussions = array();
 
         // Which course are we grabbing data for? Make sure it's an integer.
         $course = intval($course);
 
-        if (empty($course)) {
-            $course = 5;
-        }
+        // Exclude specified forums
+        $where_fora_exclude_sql = (!empty($this->config->exclude) ? ' AND d.forum NOT IN(' . join($this->config->exclude, ',') . ') ' : '');
+        $this->config->limits = array (
+            $this->config->randomposts,
+            $this->config->oldestposts,
+            $this->config->yourposts
+        );
 
         // These are the different bits in the three queries
         $queries = array(
-            'select' => array(',RAND() random', '', ',RAND() random'),
-            'where' => array(" AND d.userid <> :userid AND p.modified >= :modified",
-                             " AND d.userid <> :userid AND p.modified >= :modified",
-                             " AND d.userid = :userid AND p.modified >= :modified"),
-            'order' => array(',random', ',p.modified ASC', ',random'),
-        );
-        $params = array(
-            'courseid' => $course,
-            'userid' => $USER->id,
-            'modified' => (time() - 259200)
+            'where'  => array("AND d.userid <> {$USER->id} ", "AND d.userid <> {$USER->id} ", "AND d.userid = {$USER->id} "),
+            'order'  => array('', 'd.timemodified ASC,', 'd.timemodified ASC,'),
         );
 
         /// Do it backwards and exclude previous results
 
-        // This array holds previous discussion ids to exclude for the next
-        // query (stops duplication)
-        $post_exclude = array();
+        // This array holds already presented discussion ids to exclude for the next query (stops duplication)
+        $discussion_exclude = array();
+        
+        // Run the three queries
         for ($i = 2; $i >= 0; $i--) {
-            // No point doing the query if it's not enabled anyways
-            if (!$this->section_enabled($i)) {
+        
+            // No point doing the query if it's not enabled
+            if (!$this->config->limits[$i]>0)
                 continue;
-            }
-            
-            $sql  = 'SELECT p.id, p.subject, p.modified, p.discussion, p.userid,
-                     d.name, d.timemodified, d.usermodified, d.groupid, (COUNT(p.id) - 1) replies';
-            if (!empty($queries['select'][$i])) {
-                $sql .= $queries['select'][$i];
-            }
-            $sql .= ' FROM {forum_posts} p 
-                      JOIN {forum_discussions} d ON d.id = p.discussion
-                      WHERE d.course = :courseid';
-            if (!empty($this->config->exclude)) {
-                list($wheresql, $whereparams) = $DB->get_in_or_equal($this->config->exclude, SQL_PARAMS_NAMED, 'fparam0000', false);
-                $params = array_merge($params, $whereparams);
-                $sql .= ' AND d.forum '.$wheresql;
-            }
-            if (!empty($post_exclude)) {
-                list($wheresql, $whereparams) = $DB->get_in_or_equal($post_exclude, SQL_PARAMS_NAMED, 'dparam0000', false);
-                $params = array_merge($params, $whereparams);
-                $sql .= ' AND p.discussion '.$wheresql;
-            }
-            if (!empty($queries['where'][$i])) {
-                $sql .= $queries['where'][$i];
-            }
-            $sql .= ' GROUP BY p.discussion
-                      ORDER BY replies ASC '.$queries['order'][$i];
 
-            $this->discussions[$i] = $DB->get_records_sql($sql, $params, 0, $this->section_limit($i));
+            // If we've got excluded discussions build up the sql to exclude them
+            $where_post_exclude_sql = (!empty($discussion_exclude) ? 'AND d.id NOT IN(' . join($discussion_exclude, ',') . ')' : '');
+
+            // Building up the SQL statement from the bits and pieces above
+            $sql = "SELECT d.id, d.name, d.timemodified, d.groupid, (COUNT(p.id) - 1) AS replies
+                    FROM {forum_posts} p, {forum_discussions} d 
+                    WHERE d.course = $course
+                          $where_fora_exclude_sql
+                          $where_post_exclude_sql
+                          AND d.id = p.discussion 
+                          {$queries['where'][$i]}
+                    GROUP BY d.id, d.name, d.timemodified, d.groupid
+                    HAVING COUNT(p.id) = 1 
+                    ORDER BY {$queries['order'][$i]}replies ASC";
+            $this->discussions[$i] = $DB->get_records_sql($sql, null, 0, $this->queryLimit); // Need to limit after query to achieve random shuffle
 
             // If it didn't get any results it doesn't need any processing
             if (empty($this->discussions[$i])) {
+                unset($this->discussions[$i]);
                 continue;
             }
-
-            // Remove discussions with replies (can't be done in SQL).
-            // Have to do it this way because of the wacky keys created by
-            // using "get_records_sql"
-            $num = 0;
-            $max = count($this->discussions[$i]);
-            $discussion = end($this->discussions[$i]);
-            while($discussion->replies > 0 && $num++ < $max) {
-                array_pop($this->discussions[$i]);
-                prev($this->discussions[$i]);
+            
+            // For random posts, shuffle
+            if($i == 0) {
+                shuffle($this->discussions[$i]);
             }
+            
+            // Reduce the number of posts down to the required level
+            $this->discussions[$i] = array_slice($this->discussions[$i], 0, $this->config->limits[$i], true);
 
             // Add each discussion to the exclusion list
             reset($this->discussions[$i]);
             foreach($this->discussions[$i] as $discussion) {
-                $post_exclude[] = $discussion->discussion;
+                $discussion_exclude[] = $discussion->id;
             }
         }
 
         return $this->discussions;
     }
 
-    function section_enabled($index) {
-        return (!empty($this->config->enabled[$index]));
-    }
-
-    function section_limit($index) {
-        return (!empty($this->config->limit[$index]) ? intval($this->config->limit[$index]) : $this->defaultconfig['limit'][$index]);
-    }
-
+    //--------------------------------------------------------------------------
     function get_content() {
-        global $CFG, $USER, $DB;
+        global $COURSE, $CFG, $USER, $DB;
 
         // Don't do it more than once
         if ($this->content !== NULL) {
@@ -187,20 +156,16 @@ class block_unanswered_discussions extends block_base {
             return $this->content;
         }
 
-        // Get info about our current course
-        $course = $this->page->course;//$DB->get_record('course', array('id' => $this->page->course->id));
-
         require_once($CFG->dirroot.'/mod/forum/lib.php');   // We'll need this
 
-        // Do the data retreival. If we don't get anything, show a pretty
-        // message instead and return.
-        if (! $discussions = $this->get_data($course->id) ) {
-            $this->content->text .= '('.get_string('nounanswereddiscussions', 'block_unanswered_discussions').')';
+        // Do the data retreival. If we don't get anything, show a pretty message instead and return.
+        $discussions = $this->get_data($COURSE->id);
+        if (empty($discussions)) {
+            $this->content->text .= '<div class="block_unanswered_discussions_message">('.get_string('nounanswereddiscussions', 'block_unanswered_discussions').')</div>';
             return $this->content;
         }
 
-        /// Actually create the listing now
-
+        // Actually create the listing now
         $strftimedatetime = get_string('strftimedatetime');
         $strtitle = array(
             get_string('randomposts', 'block_unanswered_discussions'),
@@ -212,37 +177,38 @@ class block_unanswered_discussions extends block_base {
         ksort($this->discussions);
         reset($this->discussions);
 
-        // We're using a foreach here... why not?
+        // Output each section
         foreach ($this->discussions as $key => $set) {
+        
             // If this section's not enabled, or empty, skip it
-            if (!$this->section_enabled($key) || empty($set))
+            if (!$this->config->limits[$key] || empty($set))
                 continue;
 
             // Add the title for this section
-            $this->content->text .= '<h5>'.$strtitle[$key].'</h5>';
+            $this->content->text .= '<div class="block_unanswered_discussions_heading">'.$strtitle[$key].'</div>';
 
             // Make sure we get them all by resetting the array pointer
-            reset($set);
+            reset($set);       
 
             // Print each discussion
             foreach ($set as $discussion) {
                 $discussion->subject = $discussion->name;
-                $discussion->subject = format_string($discussion->subject, true, $course->id);
+                $discussion->subject = format_string($discussion->subject, true, $COURSE->id);
+                if(strlen($discussion->subject) > $this->maxSubjectLength) {
+                    $discussion->subject = substr($discussion->subject,0,$this->maxSubjectLength).'...';
+                }
 
                 $this->content->text
-                    .=  '<a href="'.$CFG->wwwroot.'/mod/forum/discuss.php?d='.$discussion->discussion.'">'
+                    .= '<div class="block_unanswered_discussions_item">'
+                    .  '<span class="block_unanswered_discussions_message"><a href="'.$CFG->wwwroot.'/mod/forum/discuss.php?d='.$discussion->id.'"></span>'
                     .  $discussion->subject
-                    .  '</a> '
-                    .  '<span class="date">'.userdate($discussion->modified, $strftimedatetime).'</span>'
-                    .  '<br />';
+                    .  '</a><br />'
+                    .  '<span class="block_unanswered_discussions_date">'.userdate($discussion->timemodified, $strftimedatetime).'</span>'
+                    .  '</div>';
             }
 
         }
 
         return $this->content;
-    }
-
-    function instance_allow_config() {
-        return true;
     }
 }
--- lang/en/block_unanswered_discussions.php
+++ lang/en/block_unanswered_discussions.php
@@ -1,14 +1,32 @@
 <?php
-$string['blocktitle'] = 'Block Title: ';
-$string['displayimages'] = 'Display Bullet Images';
-$string['excludefora'] = 'Exclude Fora: ';
-$string['excludefora_explain'] = 'On Windows, hold down the Ctrl key to select more than one forum';
-$string['noforatoexclude'] = 'There are no fora to exclude.';
-$string['nounanswereddiscussions'] = 'There are no unanswered discussions.';
-$string['oldestposts'] = 'Oldest Unanswered Discussions';
-$string['pluginname'] = 'Unanswered discussions block';
-$string['randomposts'] = 'Random Unanswered Discussions';
-$string['show'] = 'Show: ';
+
+$string['config_show'] = 'Showing Various Unanswered Discussions';
+$string['config_show_help'] = '
+<p>With this setting, you can control whether the user will see:</p>
+<ul>
+	<li>unanswered discussions the user has begin themselves,</li>
+	<li>unanswered discussions begun by others, that have been unanswered for the longest time, and/or</li>
+	<li>random unanswered discussions begun by others (and not yet shown in the block)</li>
+</ul>
+<p>The quantity of discussions from each type can be specified by number.</p>
+<p>To disable a type of message, set the number of displayed messages to None.</p>
+';
+$string['config_exclude'] = 'Excluding Forums';
+$string['config_exclude_help'] = '
+<p>There are some forums you will not want checked for unanswered postings, usually because these forums are used to broadcast messages that no-one will respond to.
+Among these are News forums, which are initially excluded by default.</p>
+<p>You can select more than one forum by holding the control key and using the mouse to select the forums containing discussions you do not want to appear in the block. (Mac users should use Command and click.)</p>
+';
+$string['excludefora'] = 'Excluded Forums';
+$string['noforatoexclude'] = 'There are no forums to exclude';
+$string['none'] = 'None';
+$string['nounanswereddiscussions'] = 'No unanswered discussions';
+$string['oldestposts'] = 'Oldest Discussions';
+$string['pluginname'] = 'Unanswered Discussions';
+$string['randomposts'] = 'Random Discussions';
+$string['show'] = 'Show';
 $string['unanswereddiscussions'] = 'Unanswered Discussions';
-$string['yourposts'] = 'Your Unanswered Discussions';
+$string['yourposts'] = 'Your Discussions';
+$string['yourpostsconfig'] = 'User\'s Discussions';
+
 ?>
